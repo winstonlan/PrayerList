80117f78ab268cb76dcd2c19e5e3c03ddf212a44
Winston Lan <winston.chunhua.lan@gmail.com>
2018-06-17 10:51:02 -0700

    Initial Commit

---
+++ PrayerList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrayerList
+//
+//  Created by Winston Lan on 6/17/18.
+//  Copyright © 2018 Winston Lan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
